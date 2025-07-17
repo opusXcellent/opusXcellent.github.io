de4599b6f8a530737542de9da45f0d3c0552ed29
Vince <vcapalbo@usa.net>
2025-07-17 17:31:17 -0400

    fvfvfv

---
+++ shared/music.js
@@ -0,0 +1,11 @@
+  const musicFiles = [
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/cinematic-inspirational-epic-music-340872.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/space-cinematic-ambient-music-340845.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/classical-soft-piano-music-346827.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/soft-calm-background-music-346811.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/romantic-wedding-background-music-357203.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/inspirational-cinematic-nature-music-357197.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/emotional-soft-piano-music-358515.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/background-music-soft-piano-338906.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/instrumental-acoustic-guitar-music-361435.mp3'
+  ];
