3ace05c4a3dd20444973ab7d8083ed45f1c7252b
Vince <vcapalbo@usa.net>
2025-07-28 17:51:16 -0400

    gg

--- shared/music.js
+++ shared/music.js
@@ -5,7 +5,7 @@
     'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/inspirational-cinematic-nature-music-357197.mp3',
     'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/emotional-soft-piano-music-358515.mp3',
     'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/background-music-soft-piano-338906.mp3',
-    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/background-music-calm-soft-338921',
-    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/piano-background-music-soft-344547',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/background-music-calm-soft-338921.mp3',
+    'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/piano-background-music-soft-344547.mp3',
     'https://myfilmarchive.s3.us-east-1.amazonaws.com/mp3/instrumental-acoustic-guitar-music-361435.mp3'
   ];
